639af13977041bb63ffb8f912efdcb1a210821a6
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2022-10-06 19:14:50 +0900

    マイグレーションを実行するようにした

--- start.sh
+++ start.sh
@@ -10,3 +10,4 @@ docker exec -it app /bin/bash -c "cd /var/www/laravel-project && composer update
 docker exec -it app /bin/bash -c "cd /var/www/laravel-project && cp .env.example .env"
 docker exec -it app /bin/bash -c "cd /var/www/laravel-project && php artisan key:generate"
 docker exec -it app /bin/bash -c "cd /var/www/laravel-project && php artisan cache:clear"
+docker exec -it app /bin/bash -c "cd /var/www/laravel-project && php artisan migrate:refresh"
