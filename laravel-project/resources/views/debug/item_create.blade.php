47a9fcf960e5f2b46dbd12220a1ad37764cb106c
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2022-12-06 15:19:10 +0900

    fix: タイポを修正

--- laravel-project/resources/views/debug/item_create.blade.php
+++ laravel-project/resources/views/debug/item_create.blade.php
@@ -8,7 +8,7 @@
 user_id:<input type="text" name="user_id"><br>
 name:<input type="text" name="name"><br>
 description:<input type="text" name="description"><br>
-npriceame:<input type="text" name="price"><br>
+price:<input type="text" name="price"><br>
 collateral:<input type="text" name="collateral"><br>
 <input type="submit" value="send">
 </form>
