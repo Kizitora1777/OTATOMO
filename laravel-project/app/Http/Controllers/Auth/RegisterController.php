813c0e69bc6abc13266b95998804f1ec3ebe252e
haku-noir <hakunoir.00@gmail.com>
2023-01-08 16:50:19 +0900

    add: user registration

--- laravel-project/app/Http/Controllers/Auth/RegisterController.php
+++ laravel-project/app/Http/Controllers/Auth/RegisterController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -68,7 +70,19 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'api_token' => str_random(60),
+            'api_token' => Str::random(60),
         ]);
+    }
+
+    public function register(Request $request){
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'api_token' => Str::random(60),
+        ]);
+        return response()->json([
+            "user" => $user
+        ], 201);
     }
 }
